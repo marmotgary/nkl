75eca27a7926dd6942b60b2839bc2bc59d2c4fa4
Arho Saku <saku.arho@dagmar.fi>
2019-03-27 17:00:53 +0200

    Merged with remote

--- nkl/urls.py
+++ nkl/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += [
+        path(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
