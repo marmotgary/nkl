da8452d4073b44a72de8e45073cb18ea6942a901
Arho Saku <saku.arho@dagmar.fi>
2019-04-16 18:49:52 +0300

    Hardcoded news

--- frontend/src/components/LoremIpsum.vue
+++ frontend/src/components/LoremIpsum.vue
@@ -2,83 +2,140 @@
   <v-container class="lorem-ipsum">
     <v-layout column>
       <v-flex>
-        <div class="headline mt-3">Uutiset</div>
-        <p class="subheading mt-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Cras pulvinar risus quis mauris interdum, in euismod nibh
-          pretium. Etiam pulvinar tincidunt dapibus. Quisque
-          sollicitudin, mauris a consequat consectetur, turpis nisl
-          sollicitudin enim, id consectetur neque neque nec metus.
-          Pellentesque dolor nisi, vulputate quis lobortis ac,
-          tincidunt et quam. Mauris pulvinar blandit nisi nec mattis.
-          Aliquam accumsan ut sem eget efficitur. Vivamus in tortor
-          gravida eros laoreet condimentum nec vel dui. Nullam quam
-          massa, ultrices eget tincidunt a, pulvinar ac libero.
-        </p>
-      </v-flex>
-      <v-flex>
-        <div class="headline mt-3">Lorem ipsum</div>
-        <p class="subheading mt-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Cras pulvinar risus quis mauris interdum, in euismod nibh
-          pretium. Etiam pulvinar tincidunt dapibus. Quisque
-          sollicitudin, mauris a consequat consectetur, turpis nisl
-          sollicitudin enim, id consectetur neque neque nec metus.
-          Pellentesque dolor nisi, vulputate quis lobortis ac,
-          tincidunt et quam. Mauris pulvinar blandit nisi nec mattis.
-          Aliquam accumsan ut sem eget efficitur. Vivamus in tortor
-          gravida eros laoreet condimentum nec vel dui. Nullam quam
-          massa, ultrices eget tincidunt a, pulvinar ac libero.
-        </p>
+        <div class="main">
+          <div>
+            <h3>Liigan henkilökohtaiset palkinnot 2019</h3>
+            <span class data-original-title>
+              <p>Perinteiseen tapaan myös yksittäisiä pelaajia palkitaan kauden ansioitumisista, ja nyt teillä arvon pelaajat on siihen mahdollisuus vaikuttaa. Palkitsemisia suoritetaan seitsemässä eri kategoriassa, jotka ovat:</p>
 
-        <p class="subheading mt-3">
-          Nullam nec massa eu est fringilla lobortis. Praesent in enim
-          in justo blandit varius. Cras placerat arcu in sapien
-          rhoncus aliquet. Sed interdum tortor et tincidunt
-          condimentum. Etiam consequat mi leo, in suscipit odio
-          scelerisque molestie. Nam et purus consequat, iaculis augue
-          vel, sagittis ligula. Vestibulum aliquet vulputate erat.
-          Phasellus id mauris mauris. Nunc a maximus dolor. Curabitur
-          ut vestibulum arcu. Curabitur non lacus justo. Cras varius a
-          magna in semper. Nulla eros ante, consectetur faucibus
-          sapien eu, rhoncus imperdiet dui. Sed viverra iaculis nunc,
-          id pulvinar massa egestas vitae.
-        </p>
+              <p>- Vuoden Haukikuningas (runsaasti tai erittäin runsaasti haukia kauden aikana heittänyt henkilö)</p>
 
-        <p class="subheading mt-3">
-          Aenean erat metus, imperdiet eget nisl laoreet, venenatis
-          ultricies ante. In interdum ante vel dictum ullamcorper.
-          Pellentesque habitant morbi tristique senectus et netus et
-          malesuada fames ac turpis egestas. Integer sit amet gravida
-          diam. Aliquam in tempor metus. Fusce pellentesque pharetra
-          sem, et luctus justo tempor dictum. Ut feugiat est sed
-          tristique egestas. Nullam posuere a nunc in blandit.
-          Vestibulum ante ipsum primis in faucibus orci luctus et
-          ultrices posuere cubilia Curae; Suspendisse laoreet ultrices
-          eros, nec malesuada enim semper sit amet. Maecenas efficitur
-          consectetur accumsan. Etiam in aliquam turpis, ut pharetra
-          nulla. Vestibulum malesuada, nulla id elementum cursus, nibh
-          dui rhoncus felis, suscipit mattis felis enim sed ex.
-          Pellentesque scelerisque aliquam lorem, vel mattis nibh
-          tincidunt ac. Suspendisse ac nibh sit amet lacus ullamcorper
-          maximus.
-        </p>
-      </v-flex>
-      <v-flex>
-        <div class="headline mt-3">Lorem ipsum</div>
-        <p class="subheading mt-3">
-          Nullam nec massa eu est fringilla lobortis. Praesent in enim
-          in justo blandit varius. Cras placerat arcu in sapien
-          rhoncus aliquet. Sed interdum tortor et tincidunt
-          condimentum. Etiam consequat mi leo, in suscipit odio
-          scelerisque molestie. Nam et purus consequat, iaculis augue
-          vel, sagittis ligula. Vestibulum aliquet vulputate erat.
-          Phasellus id mauris mauris. Nunc a maximus dolor. Curabitur
-          ut vestibulum arcu. Curabitur non lacus justo. Cras varius a
-          magna in semper. Nulla eros ante, consectetur faucibus
-          sapien eu, rhoncus imperdiet dui. Sed viverra iaculis nunc,
-          id pulvinar massa egestas vitae.
-        </p>
+              <p>- Vuoden Tulokas (ensimmäisen kauden pelaaja, joka on pelannut hyvällä tasolla ja osoittanut hyvää kyykkä-henkeä)</p>
+
+              <p>- Runkosarjan paras pelaaja ( runkosarjan paras kyykänpelaaja: joukkueen pelin edistäminen sekä korkea henkilökohtaisen pelaamisen taso )</p>
+
+              <p>- Jatkosarjan paras pelaaja ( jatkopelien paras kyykänpelaaja: joukkueen pelin edistäminen sekä korkea henkilökohtaisen pelaamisen taso )</p>
+
+              <p>- Vuoden viimeistelijä (paras yksittäisten kyykkien sekä vaikeiden linjojen poistaja; joukkueen kovan tuloksen viimeistelevä pelaaja sekä voittojen ratkaisija)</p>
+
+              <p>- Vuoden naiskyykkääjä (kauden paras naispuolinen kyykänpelaaja; joukkueen pelin edistäminen sekä korkea henkilökohtaisen pelaamisen taso)</p>
+
+              <p>- Vuoden kyykkääjä (pelaaja, joka on osoittanut kauden aikana parasta urheiluhenkeä ja herrasmiesmäistä käytöstä yhdistettynä pelin hyvään tasoon)</p>
+
+              <p>Äänestäminen tapahtuu kirjautumalla sisään kyykkä.comiin omilla tunnuksillasi, valitsemalla valikkopalkista 'Äänestä' sekä valitsemalla lempiehdokkaasi kustakin kategoriasta.</p>
+
+              <p>Jokaisessa kategoriassa on kolme (3) ehdokasta, jotka neuvosto on valinnut kapteenien ehdotuksista.</p>
+
+              <p>Äänestysaikaa on perjantaihin 29.3.2019 klo 12:00 asti.</p>
+            </span>
+          </div>
+          <div>
+            <h3>Välierät</h3>
+            <span class data-original-title>
+              <p>Liiga on edennyt välierävaiheeseen ja otteluparit ovat:</p>
+
+              <p>SICK - LePi</p>
+              <p>Dra - SFS-6016</p>
+            </span>
+          </div>
+          <div>
+            <h3>Puolivälierät</h3>
+            <span class data-original-title>
+              <p>Liigan pleijarit jatkuu tänään puolivälierillä otteluohjelman mukaisesti! Otteluparit ovat seuraavat:</p>
+              <p>W(h)y - SICK</p>
+              <p>Dra - Kalut</p>
+              <p>SSK - SFS-6016</p>
+              <p>KKK - LePi</p>
+            </span>
+          </div>
+          <div>
+            <h3>Neljännesvälieräennakko</h3>
+            <span class data-original-title>
+              <p>Kansa ei ole kysynyt, asiantuntijat toimittavat joka tapauksessa! NKL neljännesvälieräennakko bone apple tea!</p>
+
+              <p>
+                <b>Sipuli (E)i Cestä Kuumuutta – kalia_nenät</b>
+              </p>
+              <p>
+                Tässä tiukassa parivaljakossa mennään todennäköisesti aina kolmanteen otteluun asti. Eikä olisi varsinainen yllätys, jos joku peli jouduttaisiirin uusimaan tasurin vuoksi. Lähestulkoon identtistä keskitulosta viskoneet joukkueet omaavat melko erilaisen iskukyvyn: Kalijaukkojen rosteri on ehkä aavistuksen tasaisempi laadultaan, kun taas SICKistä löytyy parhaimmillaan hyvää konabomberivoimaa siivous- ja paikkotaitojen ollessa hieman heikompia. Kalialaisten ankara harjoittelu alkaa tuottamaan tulosta ratkaisevilla heitoilla ja sipulin kuumenee tutalaisilla liikaa. R-kioskin tiskille matkaaville: SICK 2.22,
+                <b>kalia 1.82</b>
+              </p>
+
+              <p>
+                <b>Mallinnetaan Haukia Laskimet Kuumina – KC Kaatuneet Pulut</b>
+              </p>
+              <p>
+                Reenimäärät tässä otteluparissa ovat hirmuiset, mutta karttu ei vieläkään läsähdä konaan vaakatasossa kummallakaan. Laten pojat (ja tytöt) ovat kiusanneet NKL-vastaavaa koko runkkarin ajan kysellen kaapin avainta kesken krapulan ja treenit alkavat tuottaa tulosta. Superin välieriin asti matkanneet matematiikan ammattilaiset laskevat itsensä parivoittoon, vaikka käsienheiluttelijat pistävätkin vastaan kolmen matsin arvoisesti. Latelaisten siivousvoima on kuitenkin kovempi ja riittää jatkopaikkaan.
+                <b>MaHaLasKu 1.67</b>, Kalut 2.50
+              </p>
+
+              <p>
+                <b>Sätkyn fundamentalisti siipi – Karttu Wiskarit</b>
+              </p>
+              <p>
+                Ei helvetti, vanhat ukot täällä asti taas, valmistuisitte jo! Säten autisti klubilla kyykkäkokemusta on kerääntynyt jo ihan kiitettävä määrä, tosin varsinainen kyykkämenestys on jäänyt hommaamatta. Sen verran menestystä kerääntyy tällä kaudella, että sähköukot menevät jatkoon tästä parista. Karttu wiskarit saattavat hyvänä päivänä venyttää ottelusarjan kolmanteen matsiin asti, mutta vihreän vallan edustajat lopulta jyräävät parista jatkoon.
+                <b>SFS-6016 1.72</b>, kW 2.38
+              </p>
+
+              <p>
+                <b>Ei heitetty kännissä äsken – Lentävä Piirakka</b>
+              </p>
+              <p>
+                Asiantuntijoilta jäi kauden alla huomioimatta vanhat kemistit, jotka olivat jopa hiipimässä runkkarin voittotaisteluun alkukaudesta. Kemistipropelli ei kuitenkaan kantanut labrahiiriä aivan kovimpaan kärkeen, mutta tästä otteluparista ropelointi riittää jatkoon. Tämän vuoden ainoa jatkoon mennyt fuksijoukkue joutuu taipumaan ropelin alle hienon ensimmäisen kauden jälkeen ja käteen jää lähinnä vain kokemusta seuraaville kausille. Toto:
+                <b>EHKÄ 1.33</b>, LePi 4,00
+              </p>
+
+              <p>
+                <b>Kyykkä Kuuluu Kaikille – Kusiturbiini</b>
+              </p>
+              <p>
+                Wattituntien keskinäisestä kohtaamisesta ei energiaa puutu (heh heh). Menestys potentiaalia (hör hör) omaavat Kookookoolaiset onnistuivat runkkarissa hienosti ja porukan keräämää liike-energiaa (hihhii) ei hevillä muuteta toiseen muotoon. Kusimutterit voivat olla kauteensa tyytyväisiä, mutta valitettavasti jatkopelit tulevat tyssäämään ensimmäiseen kierrokseen, klaanin metsästäessä välieräpaikkaa.
+                <b>KKK 1.43</b>, KusTu 3.33
+              </p>
+
+              <p>
+                <b>Hupi Ukot Interference – Skinnarilan Sika Klupi</b>
+              </p>
+              <p>
+                HUIppaajien kausi on edennyt mallikkaasti eteenpäin ja kova treenaaminen alkaa näkyä huomattavasti parempana tasaisuutena ottelutuloksissa. Sikamiästen ainoaksi konstiksi jää armoton vuvuzelakonserttti, mutta vaikutus saattaa jäädä oletettua pienemmäksi, sillä HUpIukkojen sipuli on ehtinyt viime talvesta kovettua melkoisesti. Lottovoittona voidaan pitää ottelusarjan venyttämistä kolmanteen kohtaamiseen SSK:n toimesta. Varmat rahat veikkaajille.
+                <b>HUI 1.25</b>, SSK 5.00
+              </p>
+
+              <p>
+                <b>Draconis – Nöörd höörd</b>
+              </p>
+              <p>
+                Suurella uholla kauteen lähteneet titeläiset onnistuivat kaikkien hämmästykseksi kusemaan runkkarin kolmospaikkansa ja ”Nöhön vuosi” -vakuuttelut kärsivät kovan kolauksen. Kuisma lisää heittoonsa voimaa, minkä ehtii, mutta valitettavasti se ei riitä tulostason parantamiseen. Punavalkomustat Mestaryys 2019 -lippikset joudutaan polttamaan ja vakuuttelut liigavuoden 2020 dominoinnista voivat alkaa. Kode käy heittämässä ratkaisevat poistot niin nopeasti, etteivät muut lohi käärme teurastajat edes ehdi tajuta jatkoon pääsyään.
+                <b>Dra 1.05</b>, Nöhö 20,00
+              </p>
+
+              <p>
+                <b>Metsässä tapahtuu vaikeita asioita – Warma (hauki) ykkönen</b>
+              </p>
+              <p>
+                Runkkarin voiton lähes ilman skrybbäreitä vieneet tikkamiehet marssivat ensimmäiseltä kierrokselta rutiinilla jatkoon, vaikka Jerry viskoisikin ensimmäiset pari heittoa konan ylitse. Haukinollalta olisi voinut odottaa toisella kaudella ehkä enemmänkin ja alisuorittaminen runkkarissa johtaakin heti kovimpaan mahdolliseen vastustajaan heti pleijareiden ekalla kierroksella. 3 kertaa kuuskymmentä ja darts jatkoon.
+                <b>Darts 1.11</b>, W(h)y 10.00
+              </p>
+            </span>
+          </div>
+          <div>
+            <h3>Runkosarja päätökseen!</h3>
+            <span class data-original-title>
+              <p>Runkosarja on saatu päätökseen ja pudotuspeleihin lähdetään Dartsin täydellisessä komennossa. Pleijareitten ensimmäiset otteluparit alla:</p>
+
+              <p>Darts – W(h)y</p>
+              <p>Dra – Nöhö</p>
+              <p>HUI – SSK</p>
+              <p>KKK – KusTu</p>
+              <p>EHKÄ – LePi</p>
+              <p>SFS-6016 – kW</p>
+              <p>MaHaLasKu – Kalut</p>
+              <p>SICK – kalia</p>
+
+              <p>Pleijarit alkavat tiistaina kello 17.00 gardenilla otteluohjelman mukaisesti!</p>
+            </span>
+          </div>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
