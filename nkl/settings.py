1e2d225385e59110cee35d8a84308a6c6d810c2a
Arho Saku <saku.arho@dagmar.fi>
2019-08-02 13:37:29 +0300

    Fixed cors error.

--- nkl/settings.py
+++ nkl/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ORIGIN_WHITELIST = 'localhost:8080',
+CORS_ORIGIN_ALLOW_ALL=True
 
 ADMINS = []
 MAILER_LIST = []
