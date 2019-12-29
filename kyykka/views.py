899e77ce07daab0c57dc27309f41a8753ac31019
Ville Raukola <ville.raukola@student.lut.fi>
2019-12-29 22:31:47 +0200

    Updates registration form css

--- kyykka/views.py
+++ kyykka/views.py
@@ -1,20 +1,6 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-
 from kyykka.forms import SignUpForm
-
-#
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             return redirect('complete')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'signup.html', {'form': form})
 from kyykka.models import Player
 
 
