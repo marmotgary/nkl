eff38a4f2342555d44091bc5213948d1e08aac16
Ville Raukola <ville.raukola@student.lut.fi>
2020-01-11 18:15:24 +0200

    Allowed credentials to be set with login request.
    # Conflicts:
    #	frontend/src/components/LogIn.vue
    #	nkl/settings.py

--- nkl/settings.py
+++ nkl/settings.py
@@ -139,8 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ORIGIN_WHITELIST = 'localhost:8080',
-
+# CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+]
+CORS_ALLOW_CREDENTIALS = True
 ADMINS = []
 MAILER_LIST = []
 EMAIL_HOST = ''
