8f37297582108cda041d96fed32552c2b5e28b96
Saku <sakurho@gmail.com>
2020-03-05 16:50:46 +0200

    The dropdown menu is working now with the changes to the backend.

--- frontend/src/components/Ottelut.vue
+++ frontend/src/components/Ottelut.vue
@@ -3,37 +3,38 @@
     <v-card-title class="d-flex flex-wrap-reverse">
       <div class="order-1">
         Ottelut
-        <v-select v-on:input="selectChange(`${select.src}`)" v-model="defaultSelected" style="width: 50%" color="red" :items="options">
+        <v-select  v-on:input="getMatches()" v-model="defaultSelected" style="width: 50%" color="red" :items="options">
         </v-select>
       </div>
       <div class="order-2">
         <v-text-field color="red" v-model="search" label="Search" single-line hide-details></v-text-field>             
       </div>
     </v-card-title>
-    <v-data-table :headers="headers" :items="matches" :search="search" hide-actions>
-      <template slot="no-data">
-        <v-progress-linear color="red" slot="progress" indeterminate></v-progress-linear>
-      </template>
-      <template slot="headers" class="text-xs-center"></template>
-      <template slot="items" slot-scope="props">
-        <router-link :to="'ottelu/'+props.item.id">
-          <td class="time"><a>{{ props.item.match_time | moment('YYYY-MM-DD HH:mm') }}</a></td>
-        </router-link>
-        <td class="text-xs-left">{{ props.item.field }}</td>
-        <td class="text-xs-left">{{ props.item.home_team.abbreviation }}</td>
-        <td class="text-xs-left">{{ props.item.away_team.abbreviation }}</td>
-        <td
-          class="text-xs-left"
-          v-if="props.item.home_score_total && props.item.away_score_total"
-        >{{ props.item.home_score_total + '-' + props.item.away_score_total }}</td>
-        <td v-else class="text-xs-center">-</td>
-      </template>
-      <v-alert
-        slot="no-results"
-        :value="true"
-        color="error"
-        icon="warning"
-      >Your search for "{{ search }}" found no results.</v-alert>
+    <v-data-table :headers="headers" :items="data" :search="search" hide-actions>
+        <template slot="no-data">
+          <v-progress-linear color="red" slot="progress" indeterminate></v-progress-linear>
+        </template>
+        <template slot="headers" class="text-xs-center"></template>
+        <template slot="items" slot-scope="props">
+          <router-link :to="'ottelu/'+props.item.id">
+            <td class="time"><a>{{ props.item.match_time | moment('YYYY-MM-DD HH:mm') }}</a></td>
+          </router-link>
+          <td class="text-xs-left">{{ props.item.field }}</td>
+          <td class="text-xs-left">{{ props.item.home_team.abbreviation }}</td>
+          <td class="text-xs-left">{{ props.item.away_team.abbreviation }}</td>
+          <td
+            class="text-xs-left"
+            v-if="props.item.home_score_total && props.item.away_score_total"
+          >{{ props.item.home_score_total + '-' + props.item.away_score_total }}</td>
+          <td v-else class="text-xs-center">-</td>
+        </template>
+        <v-alert
+          slot="no-results"
+          :value="true"
+          color="error"
+          icon="warning"
+        >Your search for "{{ search }}" found no results.</v-alert>
+      </div>
     </v-data-table>
   </v-card>
 </template>
@@ -57,23 +58,43 @@ export default {
                 { text: 'Vieras', value: 'away_team.abbreviation' },
                 { text: 'Tulos', sortable: false }
             ],
+            data: [],
             matches: [],
+            post_season: [],
+            regular_season: [],
             defaultSelected: 'Kaikki ottelut',
-            options: ['Kaikki ottelut','Runkosarja','Jatkosarja']
+            options: ['Kaikki ottelut','Runkosarja','Jatkosarja'],
         };
     },
     methods: {
         getMatches: function() {
-            this.$http.get('api/matches/').then(
-                function(data) {
-                    this.matches = data.body;
-                }
-            );
+          let url = 'api/matches/';
+          let i = 0;
+
+          this.$http.get(url).then(
+              function(data) {
+                  this.data = data.body;
+                  this.matches = data.body;
+                  for (let object in data.body) {
+                    if (data.body[i].post_season) {
+                      this.post_season.push(data.body[i]);
+                    } else {
+                      this.regular_season.push(data.body[i]);
+                    }
+                    i++;
+                  }
+              }
+          );
+
+          if (this.defaultSelected == "Runkosarja") {
+            this.data = this.regular_season
+          } else if (this.defaultSelected == "Jatkosarja") {
+            this.data = this.post_season
+          } else {
+            this.data = this.matches
+          }
+
         },
-        selectChange: function(jau) {
-           console.log(this.defaultSelected)
-           console.log(jau)
-        }
     },
     mounted: function() {
         this.getMatches();
