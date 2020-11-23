3213e7d0fd04fe4236485a2676f78a897d967a1b
Ville Raukola <vilde70@gmail.com>
2020-11-23 21:45:45 +0200

    Changed to different swagger package

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,27 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from api import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('docs/', views.schema_view),
     path('csrf/', views.csrf),
     path('ping/', views.ping),
@@ -13,7 +32,7 @@ urlpatterns = [
     path('logout/', views.LogoutAPI.as_view()),
     path('register/', views.RegistrationAPI.as_view()),
     path('reserve/', views.ReservePlayerAPI.as_view()),
-    path('matches/', views.MatchList.as_view()),
+    path('matches/', views.MatchList.as_view(), name='matches-list'),
     path('matches/<int:pk>', views.MatchDetail.as_view()),
     path('throws/update/<int:pk>/', views.ThrowAPI.as_view()),
 ]
