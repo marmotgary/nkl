934977e39092e8aba777b54b00d95eea246530b5
Ville Raukola <vile@pop-os.localdomain>
2020-12-07 21:15:57 +0200

    Trying to fix db connection for CI

--- nkl/settings.py
+++ nkl/settings.py
@@ -100,8 +100,8 @@ if SYSTEM_ENV == 'GITHUB_WORKFLOW':
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'nkl',
-            'USER': 'nkladmin',
-            'PASSWORD': 'password',
+            'USER': 'test',
+            'PASSWORD': 'test',
             'HOST': '127.0.0.1',
             'PORT': '3306',
             }
