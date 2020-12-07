c2a37216448a042266cf311feed73f3745317fa4
Ville Raukola <vilde70@gmail.com>
2020-12-07 21:19:54 +0200

    Trying to fix db connection for CI

--- nkl/settings.py
+++ nkl/settings.py
@@ -100,8 +100,8 @@ if SYSTEM_ENV == 'GITHUB_WORKFLOW':
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'nkl',
-            'USER': 'test',
-            'PASSWORD': 'test',
+            'USER': 'root',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
             'PORT': '3306',
             }
